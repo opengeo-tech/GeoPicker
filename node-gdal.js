1155c26962b471614d5aec307bf5d3e3b89ef373
stefanocudini <stefano.cudini@gmail.com>
2022-10-30 13:41:38 +0100

    return loc

--- node-gdal.js
+++ node-gdal.js
@@ -12,12 +12,28 @@ const point = {
 	]
 }
 
+const pointPadova= {
+	"type": "Point",
+	"coordinates": [
+		11.703113376110231,
+		45.305530526197494
+	]
+}
+
 var dataset = gdal.open(file);
 
 var band = dataset.bands.get(1);
 
 var coordinateTransform = new gdal.CoordinateTransformation(gdal.SpatialReference.fromEPSG(4326), dataset);
 
-var pt = coordinateTransform.transformPoint(point.coordinates[0], point.coordinates[1]);
+var pt = coordinateTransform.transformPoint(pointPadova.coordinates[0], pointPadova.coordinates[1]);
 
-console.log(band.pixels.get(pt.x, pt.y));
+let pixelVal;
+try {
+	pixelVal = band.pixels.get(pt.x, pt.y)
+}
+catch(err) {
+	console.log(err)
+}
+
+console.log(pixelVal);
