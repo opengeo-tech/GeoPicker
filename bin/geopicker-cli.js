2c6429eee52e8c4c2261efb3e4f7c7b418579edc
stefanocudini <stefano.cudini@gmail.com>
2022-12-09 15:25:26 +0100

    point

--- bin/geopicker-cli.js
+++ bin/geopicker-cli.js
@@ -10,8 +10,8 @@ const console = new Console(process.stderr);
 
 const {setValue, getValue, densify} = require('../lib');
 
-const fileRaster = process.argv[2] || '../data/trentino-altoadige_30m.tif';
-const fileLine = process.argv[3] || '../data/traccia_calisio_50pt.geojson';
+const fileRaster = process.argv[2];
+const fileLine = process.argv[3];
 
 
 console.time();
