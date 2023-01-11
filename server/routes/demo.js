76e1a10f20853ce179685a613b8afe66e6b0c450
stefanocudini <stefano.cudini@gmail.com>
2023-01-11 13:04:52 +0100

    demo

--- server/routes/demo.js
+++ server/routes/demo.js
@@ -2,13 +2,20 @@
 const fs = require('fs')
   , path = require('path');
 
+const noCache = {
+    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+    'Surrogate-Control': 'no-store'
+  }
+
 module.exports = async fastify => {
 
   const htmlpath = path.resolve(`${__dirname}/../../index.html`)
       , html = fs.readFileSync(htmlpath).toString();
 
   fastify.get('/test', async (req, res) => {
-    await res.type('text/html').send(html)
+    await res.headers(noCache).type('text/html').send(html)
   });
 
   fastify.addHook('onReady', async () => {
