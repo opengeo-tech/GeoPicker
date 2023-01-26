74bb9e5beeb54f1783be05e7afdbf7ea68511a57
stefanocudini <stefano.cudini@gmail.com>
2023-01-26 19:37:36 +0100

    schemas-plugin

--- server/schemas/datasets.js
+++ server/schemas/datasets.js
@@ -1,21 +1,20 @@
 
 module.exports = (S, config) => {
 
-  const datasetNames = Object.keys(config.datasets);
-
-  const dataset = S.object()
-    .prop('type', S.string().enum(['raster','vector']))
-    .prop('band', S.integer())
-    .prop('size', S.integer())
-    .prop('epsg', S.integer())
-    .prop('width', S.integer())
-    .prop('height', S.integer())
-    .prop('bbox', S.object()
-      .prop('minLon', S.number())
-      .prop('minLat', S.number())
-      .prop('maxLon', S.number())
-      .prop('maxLat', S.number())
-    );
+  const datasetNames = Object.keys(config.datasets)
+      , dataset = S.object()
+        .prop('type', S.string().enum(['raster','vector']))
+        .prop('band', S.integer())
+        .prop('size', S.integer())
+        .prop('epsg', S.integer())
+        .prop('width', S.integer())
+        .prop('height', S.integer())
+        .prop('bbox', S.object()
+          .prop('minLon', S.number())
+          .prop('minLat', S.number())
+          .prop('maxLon', S.number())
+          .prop('maxLat', S.number())
+        );
 
   return {
     dataset: {
