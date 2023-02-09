8a10032fc131c04d4d61423736daca52f51956d8
stefanocudini <stefano.cudini@gmail.com>
2023-02-09 21:06:09 +0100

    status-link-doc

--- server/routes/status.js
+++ server/routes/status.js
@@ -1,14 +1,19 @@
 
+const {resolve} = require('path');
+
 module.exports = async fastify => {
 
   const {config, status, gpicker, package} = fastify
-      , {version} = package
-      , {attribution} = config
+      , {version, homepage} = package
+      , {attribution, swagger} = config
+
+  const documentation = swagger.enabled ? resolve(swagger.routePrefix) : homepage;
 
   fastify.get('/status', async () => ({
     name: 'GeoPicker',
     version,
     gdal: gpicker.gdal.version,
+    documentation,
     attribution,
     status
   }));
